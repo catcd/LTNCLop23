775fc635875756fd8dbd1d9faf62b0483917425b
Duy-Cat Can (W) <catcd@vnu.edu.vn>
2021-03-17 09:39:48 +0700

    add method 2 of reverse

--- week7_example1.cpp
+++ week7_example1.cpp
@@ -1,9 +1,15 @@
 void reverse(char *s) {
-    char *begin = s, *last = s + strlen(s) - 1;
+    char *begin = s;
+    char *last = s + strlen(s) - 1;
     
     while (begin < last) {
         swap(*begin, *last);
         begin++;
         last--;
     }
+}
+
+// method 2
+for (char *begin = s, *last = s + strlen(s) - 1; begin < last; begin++, last--) {
+    swap(*begin, *last);
 }
